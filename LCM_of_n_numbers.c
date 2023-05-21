b716eb66d898f623ecc9d083ad675dc0932e81ba
srujana <119722973+srujana-balam@users.noreply.github.com>
2023-05-21 06:50:12 +0530

    #include<stdio.h> int main() {     int n,i,max=-100;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]>max)         {             max=arr[i];         }     }     while(1)     {         int times=0;         for(i=0;i<n;i++)         {             if(max%arr[i]==0)             {                 times+=1;             }         }         if(times==n)         {             printf("%d",max);             break;         }         else         {             max+=1;         }     } }

---
+++ LCM_of_n_numbers.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,max=-100;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    while(1)
+    {
+        int times=0;
+        for(i=0;i<n;i++)
+        {
+            if(max%arr[i]==0)
+            {
+                times+=1;
+            }
+        }
+        if(times==n)
+        {
+            printf("%d",max);
+            break;
+        }
+        else
+        {
+            max+=1;
+        }
+    }
+}
