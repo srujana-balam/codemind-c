87380e3c35b5e3da47932f9883e3d1a9bb4ad478
srujana <119722973+srujana-balam@users.noreply.github.com>
2023-05-22 21:11:51 +0530

    #include<stdio.h> int main() {     int v[2004],r,k,a,m=0;     scanf("%d",&r);     for (k=0;k<r;k++)     {         scanf("%d",&v[k]);     }     for(k=0;k<r;k++)     {         if(v[k]%2==0)         {             break;         }         else         {             m+=v[k];         }     }     printf("%d",m); }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int v[2004],r,k,a,m=0;
+    scanf("%d",&r);
+    for (k=0;k<r;k++)
+    {
+        scanf("%d",&v[k]);
+    }
+    for(k=0;k<r;k++)
+    {
+        if(v[k]%2==0)
+        {
+            break;
+        }
+        else
+        {
+            m+=v[k];
+        }
+    }
+    printf("%d",m);
+}
