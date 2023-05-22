e1f2bf484234b26e09212ffd427155f40176df2c
srujana <119722973+srujana-balam@users.noreply.github.com>
2023-05-22 19:19:13 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }
    }

---
+++ Array_of_odd_and_even_elements.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    
+}
